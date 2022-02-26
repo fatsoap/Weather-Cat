81c933bab0412a853f5e59c0e24f135068d99c25
fatsoap <fatsoap429@icloud.com>
2022-02-26 18:49:34 +0800

    init Serverless Go App

---
+++ weather-linebot/go.mod
@@ -0,0 +1,8 @@
+module weather-linebot
+
+go 1.17
+
+require (
+	github.com/aws/aws-lambda-go v1.28.0 // indirect
+	github.com/line/line-bot-sdk-go/v7 v7.13.0 // indirect
+)
---
+++ weather-linebot/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/line/line-bot-sdk-go/v7/linebot"
+)
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		Body:       fmt.Sprintf("Hello, %v", string("Sheep")),
+		StatusCode: 200,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
